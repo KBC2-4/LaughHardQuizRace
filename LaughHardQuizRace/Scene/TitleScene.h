abbfe37cb0978cf74ca4b330f8476f52d669d743
nakamurakouki4 <122337032+nakamurakouki4@users.noreply.github.com>
2024-02-14 18:47:06 +0900

    タイトル更新

--- LaughHardQuizRace/Scene/TitleScene.h
+++ LaughHardQuizRace/Scene/TitleScene.h
@@ -11,6 +11,9 @@ private:
 private:
 	int background_image;
 	int menu_image;
+	int title_image;
+	int back_ground;     //”wŒi‰æ‘œ
+	int scroll;         //‘–s‹——£
 	int cursor_image;
 	int menu_cursor;
 	int play_count;             //ƒvƒŒƒC‰ñ”
